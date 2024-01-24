fcff355e5acef7902309f8e83f7d45f55ee1435c
keonly <keon.lee@proton.me>
2024-01-24 16:55:12 +0900

    feat: solve p02775

---
+++ boj/src/bin/p02775.rs
@@ -0,0 +1,29 @@
+use std::fmt::Write;
+use std::io::{stdin, read_to_string};
+
+fn combination(n: usize, k: usize) -> usize {
+    if k > n {
+        0
+    } else {
+        (1..=k).fold(1, |acc, val| acc * (n + 1 - val) / val)
+    }
+}
+
+fn main() {
+    let input = read_to_string(stdin()).unwrap();
+    let mut output = String::new();
+
+    let mut nums = input
+        .split_ascii_whitespace()
+        .map(|c| c.parse::<usize>().unwrap());
+
+    let tc: usize = nums.next().unwrap();
+    for i in 0..tc {
+        let k: usize = nums.next().unwrap();
+        let n: usize = nums.next().unwrap();
+
+        write!(output, "{}\n", combination(n + k, k + 1)).unwrap();
+    }
+
+    print!("{}", output);
+}
