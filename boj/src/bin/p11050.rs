fb3059917351c2a90ffe5faf486a9b6dbe0cc190
keonly <keon.lee@proton.me>
2024-01-25 21:48:12 +0900

    feat: solve p11050

---
+++ boj/src/bin/p11050.rs
@@ -0,0 +1,25 @@
+use std::fmt::Write;
+use std::io::{read_to_string, stdin};
+
+fn combination(n: usize, k: usize) -> usize {
+    if k > n {
+        0
+    } else {
+        (1..=k).fold(1, |acc, val| acc * (n + 1 - val) / val)
+    }
+}
+
+fn main() {
+    let input = read_to_string(stdin()).unwrap();
+    let mut output = String::new();
+
+    let mut nums = input
+        .split_ascii_whitespace()
+        .map(|c| c.parse::<usize>().unwrap());
+
+    let n: usize = nums.next().unwrap();
+    let k: usize = nums.next().unwrap();
+
+    writeln!(output, "{}", combination(n, k)).unwrap();
+    print!("{}", output);
+}
