3ab3dea314fda50fb7c2a322ffaebe708bcc726f
keonly <keon.lee@proton.me>
2024-01-26 17:33:53 +0900

    feat: solve p02869

---
+++ boj/src/bin/p02869.rs
@@ -0,0 +1,21 @@
+use std::fmt::Write;
+use std::io::{read_to_string, stdin};
+
+fn main() {
+    let input = read_to_string(stdin()).unwrap();
+    let mut output = String::new();
+
+    let nums: Vec<usize> = input
+        .trim()
+        .split_ascii_whitespace()
+        .map(|c| c.parse::<usize>().unwrap())
+        .collect();
+
+    write!(
+        output,
+        "{}",
+        (nums[2] + nums[0] - 2 * nums[1] - 1) / (nums[0] - nums[1])
+    )
+    .unwrap();
+    print!("{}", output);
+}
