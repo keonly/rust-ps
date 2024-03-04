3f11b2c936e09951b7b5dbcbf17298171955c1ec
keonly <keon.lee@proton.me>
2024-03-04 17:06:48 +0900

    feat: solve p01697

--- boj/src/bin/p01697.rs
+++ boj/src/bin/p01697.rs
@@ -2,35 +2,27 @@ use std::collections::{HashSet, VecDeque};
 use std::fmt::Write;
 use std::io::{read_to_string, stdin};
 
-fn hide_and_seek(n: usize, k: usize) -> usize {
-    let mut visiting: VecDeque<usize> = VecDeque::new();
-    let mut visited: HashSet<usize> = HashSet::new();
-    let mut steps: usize = 0;
+fn hide_and_seek(n: isize, k: isize) -> isize {
+    let mut visiting: VecDeque<(isize, isize)> = VecDeque::new();
+    let mut visited: HashSet<isize> = HashSet::new();
 
-    visiting.push_back(n);
+    visiting.push_back((n, 0));
 
-    'outer: loop {
-        let temp = visiting.clone();
-        let curr_step = temp.iter().to_owned();
-        visiting.clear();
-
-        for curr in curr_step {
-            if *curr == k {
-                break 'outer;
-            }
-            if visited.contains(curr) || *curr > 200_000 {
-                continue;
-            }
-            visited.insert(*curr);
-            for next in [curr - 1, curr + 1, curr * 2] {
-                visiting.push_back(next);
-            }
+    while let Some((curr_pos, curr_step)) = visiting.pop_front() {
+        if curr_pos == k {
+            return curr_step;
         }
 
-        steps += 1;
+        visited.insert(curr_pos);
+        for next_pos in [curr_pos - 1, curr_pos + 1, curr_pos * 2] {
+            if visited.contains(&next_pos) || next_pos > 200_000 {
+                continue;
+            }
+            visiting.push_back((next_pos, curr_step + 1))
+        }
     }
 
-    steps
+    isize::MAX
 }
 
 fn main() {
@@ -38,7 +30,7 @@ fn main() {
     let mut input = buffer.split_ascii_whitespace();
     let mut output = String::new();
 
-    let [n, k] = [(); 2].map(|_| input.next().unwrap().parse::<usize>().unwrap());
+    let [n, k] = [(); 2].map(|_| input.next().unwrap().parse::<isize>().unwrap());
 
     write!(output, "{}", hide_and_seek(n, k)).unwrap();
     print!("{output}");
