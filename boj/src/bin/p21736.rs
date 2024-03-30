c83be9860db83f6d02782c6319f403bb028b0133
keonly <keon.lee@proton.me>
2024-03-30 18:45:30 +0900

    feat: solve p21736

--- boj/src/bin/p21736.rs
+++ boj/src/bin/p21736.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashSet, VecDeque};
+use std::collections::VecDeque;
 use std::fmt::Write;
 use std::io::{read_to_string, stdin};
 
@@ -13,31 +13,26 @@ impl Campus {
         0 <= row && row < self.rows as isize && 0 <= col && col < self.cols as isize
     }
 
-    fn count_reachable_people(&self, start_coord: (usize, usize)) -> usize {
+    fn count_reachable_people(&mut self, start_coord: (usize, usize)) -> usize {
         let mut visiting: VecDeque<(usize, usize)> = VecDeque::new();
-        let mut visited: HashSet<(usize, usize)> = HashSet::new();
         let directions: [(isize, isize); 4] = [(0, 1), (0, -1), (1, 0), (-1, 0)];
         let mut count_people = 0;
         visiting.push_back(start_coord);
-        visited.insert(start_coord);
 
         while let Some((curr_row, curr_col)) = visiting.pop_front() {
             for (d_row, d_col) in directions {
                 let (next_row, next_col) = (curr_row as isize + d_row, curr_col as isize + d_col);
+
                 if self.in_range(next_row, next_col) {
                     let (next_row, next_col) = (next_row as usize, next_col as usize);
-                    if !visited.contains(&(next_row, next_col)) {
-                        visited.insert((next_row, next_col));
-                        let c = self.map[next_row][next_col];
-                        match c {
-                            'X' => {}
-                            _ => {
-                                visiting.push_back((next_row, next_col));
-                                if c == 'P' {
-                                    count_people += 1;
-                                }
-                            }
+                    let c = self.map[next_row][next_col];
+
+                    if c != 'X' {
+                        visiting.push_back((next_row, next_col));
+                        if c == 'P' {
+                            count_people += 1;
                         }
+                        self.map[next_row][next_col] = 'X';
                     }
                 }
             }
@@ -54,7 +49,7 @@ fn main() {
 
     let [rows, cols] = [(); 2].map(|_| input.next().unwrap().parse::<usize>().unwrap());
     let mut start_coord: Option<(usize, usize)> = None;
-    let map: Vec<Vec<char>> = (0..rows)
+    let mut map: Vec<Vec<char>> = (0..rows)
         .map(|row_idx| {
             input
                 .next()
@@ -70,18 +65,13 @@ fn main() {
                 .collect::<Vec<char>>()
         })
         .collect();
-    let campus = Campus { rows, cols, map };
+    let mut campus = Campus { rows, cols, map };
     let count_people = campus.count_reachable_people(start_coord.unwrap());
 
-    write!(
-        output,
-        "{}",
-        if count_people > 0 {
-            count_people.to_string()
-        } else {
-            "TT".to_string()
-        }
-    )
-    .unwrap();
+    if count_people > 0 {
+        write!(output, "{count_people}").unwrap();
+    } else {
+        write!(output, "TT").unwrap();
+    }
     print!("{output}");
 }
